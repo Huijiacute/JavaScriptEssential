d9476dc4ddb88c7202549325f791b13053864ccb
Huijiacute <limhouykea@gmail.com>
2026-03-05 22:51:43 -0500

    .

---
+++ Module2/restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((items, index) => `<p>Item ${index + 1}: ${items}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`
+})
+document.getElementById('maincourseManuItems').innerHTML = mainCourseItem;
+
+let dessertItems = dessertMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('')
+document.getElementById('dessertMenuItems').innerHTML = dessertItems
+
