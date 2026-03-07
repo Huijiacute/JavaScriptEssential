b5c6fafe52498fa2f6ea9ecaaee860b7cc144e28
Huijiacute <limhouykea@gmail.com>
2026-03-07 03:47:29 -0500

    .

--- Module2/employeeDetails/employee_details.js
+++ Module2/employeeDetails/employee_details.js
@@ -4,6 +4,8 @@ const employees = [
     { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
     //... More employee records can be added here
   ];  
+
+//display employees
 const displayEmployees = () => {
   const totalEmployees = employees.map(employee => `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
   document.getElementById('employeesDetails').innerHTML = totalEmployees
@@ -11,3 +13,32 @@ const displayEmployees = () => {
 
 const displayEmployeeInHTML = document.getElementById('displayEmployees')
 displayEmployeeInHTML.addEventListener('click', displayEmployees)
+
+//calculate total salaries
+const calculateTotalSalaries = () => {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: $${totalSalaries}`);
+}
+
+const calculateSalaries = document.getElementById('calculateTotalSalaries')
+calculateSalaries.addEventListener('click', calculateTotalSalaries)
+
+//Display HR employees
+const displayHREmployees = () => {
+    const hrEmployees = employees.filter(employee => employee.department === "HR")
+    const hrEmployeesDisplay = hrEmployees.map(employee => `<p>${employee.id}: ${employee.name}:  ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
+    document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+
+const hrDisplay = document.getElementById('displayHREmployees')
+hrDisplay.addEventListener('click', displayHREmployees)
+
+const findEmployeeById = () => {
+    const employeeId = prompt(parseInt("Enter employee ID: "))
+    const foundEmployee = employees.find(employee => employee.id === employeeId)
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`
+    } else {
+        document.getElementById('employeesDetails').innerText = 'no employee has been found with this ID';
+    }
+}
