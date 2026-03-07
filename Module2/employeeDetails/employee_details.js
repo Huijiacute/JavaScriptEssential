b13e24e707cdef7911e8c78c20abb793a8a07d64
Huijiacute <limhouykea@gmail.com>
2026-03-07 03:16:27 -0500

    new project

---
+++ Module2/employeeDetails/employee_details.js
@@ -0,0 +1,14 @@
+const employees = [
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    //... More employee records can be added here
+  ];
+
+const displayEmployeeInHTML = document.getElementById('displayEmployees')
+displayEmployeeInHTML.addEventListener('click', displayEmployees)
+
+const displayEmployees = () => {
+    const totalEmployees = employees.map(employee => `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees
+}
